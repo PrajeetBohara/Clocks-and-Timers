ae628d51f3f9c7ff726a20be2735d5fa07528919
Prajeet Bohara <153901646+PrajeetBohara@users.noreply.github.com>
2024-12-28 19:39:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 from tkinter import *
 import time
 
-PURPLE = "#7e5cad"
-RED = "#e7305b"
-GREEN = "#9bdeac"
-YELLOW = "#f7f5dd"
-FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+FONT ="Baskerville"
 
 window = Tk()
 window.title("Timer")
@@ -23,8 +16,10 @@ def timer(hour, minute, second):
     formatted_hour = f"{hour:02d}"
     canvas.itemconfig(time_text, text=f"{formatted_hour}:{formatted_minute}:{formatted_second}")
     if hour == 0 and minute == 0 and second == 0:
+        #here this statement checks first before going over other if statements so that if the hour
+        #minute and second are zero then the function exits
         return #exit this function without returning anything
-    if minute == 0 and hour > 0 and second == 0:
+    if hour > 0 and minute == 0 and second == 0:
         hour -= 1
     if hour == 1 and minute == 0 and second == 0:
         hour -= 1
@@ -39,7 +34,7 @@ canvas = Canvas(width=300, height=200)#create a canvas of specific width and hei
 image = PhotoImage(file="Halloween.png") #we need PhotoImage class to import the images and handle them
 canvas.create_image(150,100, image=image) #here in parameters the given values are the x,y position
 
-time_text = canvas.create_text(150,120, text=f"00:00:00", fill="red", font=(FONT_NAME, 35, "bold"))
+time_text = canvas.create_text(150,120, text=f"00:00:00", fill="red", font=(FONT, 35, "bold"))
 canvas.create_text(150,85, text="Timer", fill="red", font=("Roman", 15))
 #this creates the text in canvas at specific location with fill color and the font
 canvas.pack() #to display the canvas
